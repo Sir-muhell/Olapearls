0817e6635a66caa53f9a33200826ae5f69d2fd27
Aholu Samuel <samuelaholu15@gmail.com>
2022-02-14 18:20:55 +0100

    Update single.php

--- single.php
+++ single.php
@@ -2,7 +2,7 @@
 <?php //code to get the item using its id
 include("database/conn.php");//database config file
 $id=$_REQUEST['id'];
-$link  = str_replace('-', ' ', $link);
+$link  = str_replace('-', ' ', $id);
 $query="SELECT * from blogs where id='".$link."'"; $result=mysqli_query($GLOBALS["___mysqli_ston"],$query) or die ( ((is_object($GLOBALS["___mysqli_ston"]))? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ?$___mysqli_res : true))); 
 $row = mysqli_fetch_assoc($result);
 //pageview count query
