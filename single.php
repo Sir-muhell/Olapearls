34e958753fb77f5952f697dd4fd06c07f87352a4
Aholu Samuel <samuelaholu15@gmail.com>
2022-02-14 19:33:37 +0100

    Update single.php

--- single.php
+++ single.php
@@ -4,7 +4,7 @@ include("database/conn.php");//database config file
 $id=$_REQUEST['id'];
 $link  = str_replace('-', ' ', $id);
 echo $link;
-$query="SELECT * from blogs where title LIKE '".$link.%"'"; $result=mysqli_query($GLOBALS["___mysqli_ston"],$query) or die ( ((is_object($GLOBALS["___mysqli_ston"]))? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ?$___mysqli_res : true))); 
+$query="SELECT * from blogs where title LIKE '".$link."%'"; $result=mysqli_query($GLOBALS["___mysqli_ston"],$query) or die ( ((is_object($GLOBALS["___mysqli_ston"]))? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ?$___mysqli_res : true))); 
 $row = mysqli_fetch_assoc($result);
 //pageview count query
 $page=$row['title'];
