6f68e17f29a079ee869bc262f7db30c37694b113
Aholu Samuel <samuelaholu15@gmail.com>
2022-02-14 18:27:29 +0100

    Update single.php

--- single.php
+++ single.php
@@ -2,6 +2,7 @@
 <?php //code to get the item using its id
 include("database/conn.php");//database config file
 $id=$_REQUEST['id'];
+echo $id;
 $link  = str_replace('-', ' ', $id);
 echo $link;
 $query="SELECT * from blogs where title='".$link."'"; $result=mysqli_query($GLOBALS["___mysqli_ston"],$query) or die ( ((is_object($GLOBALS["___mysqli_ston"]))? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ?$___mysqli_res : true))); 
