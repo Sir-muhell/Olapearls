751013ab594ecdcb18eaa27052dd753465008011
Aholu Samuel <samuelaholu15@gmail.com>
2022-02-14 22:17:33 +0100

    Update fetch_data.php

--- libs/fetch_data.php
+++ libs/fetch_data.php
@@ -371,7 +371,7 @@ function getcategoriesmenu($table)
 			// echo '<a class="dropdown-item" href="category.php?id='.$category['id'].'">'.$category['name'].'</a>
 			// <div class="dropdown-divider"></div>';
 
-			echo'<li><a href="category/'.$link.'"><i class="fa fa-angle-double-right" aria-hidden="true"></i>'.$category['name'].'</li>';
+			echo'<li><a href="category/'.$link.'"><i class="fa fa-angle-double-right" aria-hidden="true"></i>'.$category['name'].'</li></a>';
 		}
 	}
 
