2a235768da10fa9f1a8452bd34a6cd38748e8dbb
Aholu Samuel <samuelaholu15@gmail.com>
2022-02-16 11:23:42 +0100

    Update category.php

--- cat/category.php
+++ cat/category.php
@@ -83,7 +83,7 @@ $query="SELECT * from blog_categories WHERE `name` = '$link'";
 $result=mysqli_query($GLOBALS["___mysqli_ston"],$query) or die ( ((is_object($GLOBALS["___mysqli_ston"]))? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ?$___mysqli_res : true))); 
 $row = mysqli_fetch_assoc($result);
 
-if ($id == '' || $result == '') {
+if ($id == '' || $row == '') {
  header("location: ./oops ");
 }
 
