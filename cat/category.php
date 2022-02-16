dd540bd1c24c13cd855f1f3c44206296b3cf4501
Aholu Samuel <samuelaholu15@gmail.com>
2022-02-16 11:21:37 +0100

    Update category.php

--- cat/category.php
+++ cat/category.php
@@ -79,7 +79,7 @@
 include("../database/conn.php");//database config file
 $id=$_REQUEST['name'];
 $link  = str_replace('-', ' ', $id);
-$query="SELECT * from blog_categories where `name` = '%$link%'";
+$query="SELECT * from blog_categories WHERE `name` = '$link'";
 $result=mysqli_query($GLOBALS["___mysqli_ston"],$query) or die ( ((is_object($GLOBALS["___mysqli_ston"]))? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ?$___mysqli_res : true))); 
 $row = mysqli_fetch_assoc($result);
 
